2f4757be845d426821ec4db138de46942cbcb8da
cpmoore0317 <cpmoore317@gmail.com>
2024-09-12 10:55:14 -0500

    Created PlayListItem test

---
+++ src/__tests__/PlayListItem.test.tsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+import PlayListItem from '@/components/PlayListItem';
+import { expect, it, describe } from 'vitest';
+
+describe('PlayListItem Component', () => {
+    it('renders correctly when selected', () => {
+        const { container } = render(
+            <PlayListItem
+                title='Test Song'
+                artist='Test Artist'
+                duration='3:30'
+                isSelected={true}
+                onClick={() => {}}
+            />    
+        );
+        expect(container).toMatchSnapshot();
+    });
+
+    it('renders correctly when not selected', () => {
+        const { container } = render(
+            <PlayListItem
+                title="Another Song"
+                artist="Another Artist"
+                duration="4:00"
+                isSelected={false}
+                onClick={() => {}}
+            />
+        );
+        expect(container).toMatchSnapshot();
+    });
+});
